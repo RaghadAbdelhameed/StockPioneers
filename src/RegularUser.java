93877a06fc79e46c0c2b5fd1bea039e985407c90
RaghadAbdelhameed <Raghad.Abdelhameed04@eng-st.cu.edu.eg>
2024-05-04 21:30:26 +0300

    Rawan's Code

--- src/RegularUser.java
+++ src/RegularUser.java
@@ -7,14 +7,68 @@ public class RegularUser extends User {
 	private boolean isWithdrawalApproved;
 
 	private List<Transaction> pendingTransactions; // List to store pending deposit and withdrawal requests
+	private List<Stock> requestedStocks;
+    private List<Stock> haveStocks;
+    private List<Stock> Stocks;
+
 
 	public RegularUser(String username, String password, int ID, double accountbalance, gender gender) {
 		super(username, password, ID, accountbalance, gender);
 		this.isDepositApproved = false; // Admin doesnot approved by default
 		this.isWithdrawalApproved = false;
+		this.requestedStocks = new ArrayList<>(); // list for the requested stocks
+        this.haveStocks = new ArrayList<>(); // list for the stocks that User have
+        this.Stocks = new ArrayList<>();
 		pendingTransactions = new ArrayList<>();
 	}
+ // 2-Order Mangement
 
+    // Add Stocks Order
+    public void addStocksOrder(String label, String company, double maxPrice) {
+        for (Stock stock : Stocks) {
+            if (stock.getLabel().equals(label) && stock.getCompany().equals(company)) {
+                requestedStocks.add(stock);
+                System.out.println(" Stock order added ");
+                buyStocksOrder(maxPrice);
+            }
+        }
+        System.out.println(" Stock Not Found "); // unreachable code when (return stock)
+    }
+
+    // Delete Stocks Order
+    public void delelteStocksOrder(String label, String company) {
+        requestedStocks.removeIf(stock -> stock.getLabel().equals(label) && stock.getCompany().equals(company));
+    }
+
+    // Buy Stocks Order
+    public void buyStocksOrder(double maxPrice) {
+        for (Stock stock : requestedStocks) {
+            if (stock.getTradingPrice() <= maxPrice) { // Option to automatically execute buy Stock if it's
+                                                       // available
+                setAccountBalance(getAccountBalance() - stock.getTradingPrice());
+                haveStocks.add(stock);
+                requestedStocks.remove(stock);
+                System.out.println(" Stock bought successfully");
+            }
+            System.out.println(" Stock is not available at or below the requested price ");
+        }
+    }
+
+    // Sell Stocks Order
+    public void sellStocksOrder(String label, String company, double minPrice) {// adding prices
+        for (Stock stock : haveStocks) {
+            if (stock.getLabel().equals(label) && stock.getCompany().equals(company)) {
+                if (stock.getTradingPrice() >= minPrice) {
+                    setAccountBalance(getAccountBalance() + stock.getTradingPrice());
+                    haveStocks.remove(stock);
+                    System.out.println(" Stock sold successfully");
+                }
+            }
+        }
+    }
+
+
+	//3- Deposit/Withdrawal
 	// Method to deposit funds (pending admin approval)
 	public void deposit(double amount) { // deposit request
 		if (amount > 0) { // to be valid
