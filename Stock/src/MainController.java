b34926cc329ffbe30dc0102ffd8c350cc19b17fa
rawan-mohamed-n <Rawan.Tawab04@eng-st.cu.edu.eg>
2024-05-10 06:10:55 +0300

    added admin's stock management & user's order management

--- Stock/src/MainController.java
+++ Stock/src/MainController.java
@@ -17,7 +17,7 @@ import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.lang.classfile.Label;
+//import java.lang.classfile.Label;
 import java.net.URL;
 import java.util.Optional;
 import java.util.ResourceBundle;
@@ -89,6 +89,9 @@ public class MainController {
     private Button userManageButton;
 
     @FXML
+    private Button stockManageButton;
+
+    @FXML
     void adminLogoutButtonClicked(ActionEvent event) {
         // Create a confirmation alert
         Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
@@ -114,6 +117,11 @@ public class MainController {
     @FXML
     void userManageClicked(ActionEvent event) {
         loadFXML("UserManage.fxml", userManageButton, "User Management");
+    }
+
+    @FXML
+    void stockManageClicked(ActionEvent event) {
+        loadFXML("StockManage.fxml", stockManageButton, "Stock Management");
     }
 
     // User Management
@@ -247,8 +255,8 @@ public class MainController {
     @FXML
     private TextField searchIDfield;
 
-    @FXML
-    private Label searchResult;
+    // @FXML
+    // private Label searchResult;
 
     @FXML
     private Button searhButton;
@@ -278,6 +286,157 @@ public class MainController {
     }
 
     // Retrive User
+
+    // Stock Management
+    @FXML
+    private Button createStockButton;
+
+    @FXML
+    private Button retriveStockButton;
+
+    @FXML
+    private Button updateStockButton;
+
+    @FXML
+    private Button deleteStockButton;
+
+    @FXML
+    void createStockClicked(ActionEvent event) {
+        loadFXML("CreateStock.fxml", createStockButton, "Create Stock");
+    }
+
+    @FXML
+    void retriveStockClicked(ActionEvent event) {
+        loadFXML("RetrieveStock.fxml", retriveStockButton, "Retrive Stock");
+    }
+
+    @FXML
+    void updateStockClicked(ActionEvent event) {
+        loadFXML("UpdateStock.fxml", updateStockButton, "Update Stock");
+    }
+
+    @FXML
+    void deleteStockClicked(ActionEvent event) {
+        loadFXML("DeleteStock.fxml", deleteStockButton, "Delete Stock");
+    }
+
+    // Create Stock
+    private Stock stock; // User object for creating or updating users
+
+    public void setStock(Stock stock) {
+        this.stock = stock; // Reference to the Admin instance
+    }
+
+    @FXML
+    private TextField createAvailableStocksField;
+
+    @FXML
+    private TextField createDividendsField;
+
+    @FXML
+    private TextField createInitialPriceField;
+
+    @FXML
+    private TextField createProfitPercentageField;
+
+    @FXML
+    private TextField createStockCompanyField;
+
+    @FXML
+    private TextField createStockLabelField;
+
+    @FXML
+    private TextField createTradingPriceField;
+
+    @FXML
+    private Button stockCreateButton;
+
+    @FXML
+    void stockCreateClicked(ActionEvent event) {
+        String label = createStockLabelField.getText();
+        String company = createStockCompanyField.getText();
+        double initialPrice = Double.parseDouble(createInitialPriceField.getText());
+        double tradingPrice = Double.parseDouble(createTradingPriceField.getText());
+        double dividends = Double.parseDouble(createDividendsField.getText());
+        double profitPercentage = Double.parseDouble(createProfitPercentageField.getText());
+        int availableStocks = Integer.parseInt(createAvailableStocksField.getText());
+        stock.setLabel(label);
+        stock.setCompany(company);
+        stock.setInitialPrice(initialPrice);
+        stock.setTradingPrice(tradingPrice);
+        stock.setDividends(dividends);
+        stock.setProfitPercentage(profitPercentage);
+        stock.setAvailableStocks(availableStocks);
+        admin.createStock(stock);
+    }
+
+    // Update Stock
+    @FXML
+    private Button searchStockButton;
+
+    @FXML
+    private Button stockUpdateButton;
+
+    @FXML
+    private TextField updateAvailableStocksField;
+
+    @FXML
+    private TextField updateDividendsField;
+
+    @FXML
+    private TextField updateInitialPriceField;
+
+    @FXML
+    private TextField updateProfitPercentageField;
+
+    @FXML
+    private TextField updateStockCompanyField;
+
+    @FXML
+    private TextField updateStockLabelField;
+
+    @FXML
+    private TextField updateTradingPriceField;
+
+    @FXML
+    void searchStockClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void stockUpdateClicked(ActionEvent event) {
+        String label = createStockLabelField.getText();
+        String company = createStockCompanyField.getText();
+        double initialPrice = Double.parseDouble(createInitialPriceField.getText());
+        double tradingPrice = Double.parseDouble(createTradingPriceField.getText());
+        double dividends = Double.parseDouble(createDividendsField.getText());
+        double profitPercentage = Double.parseDouble(createProfitPercentageField.getText());
+        int availableStocks = Integer.parseInt(createAvailableStocksField.getText());
+        stock.setLabel(label);
+        stock.setCompany(company);
+        stock.setInitialPrice(initialPrice);
+        stock.setTradingPrice(tradingPrice);
+        stock.setDividends(dividends);
+        stock.setProfitPercentage(profitPercentage);
+        stock.setAvailableStocks(availableStocks);
+        admin.updateStock(stock);
+
+    }
+
+    // Delete Stock
+    @FXML
+    private Button stockDeleteButton;
+
+    @FXML
+    private TextField searchLabelfield;
+
+    @FXML
+    void stockDeleteClicked(ActionEvent event) {
+        String label = searchLabelfield.getText();
+        stock.setLabel(label);
+        admin.deleteStock(label);
+
+    }
 
     // User Login
     @FXML
@@ -314,6 +473,84 @@ public class MainController {
     public void initialize(URL location, ResourceBundle resources) {
         // Initialize gender ComboBox items
         genderCombobox.getItems().addAll("Male", "Female");
+    }
+
+    // User Features
+    @FXML
+    private Button orderManageButton;
+
+    @FXML
+    void orderManageClicked(ActionEvent event) {
+        loadFXML("OrderManage.fxml", orderManageButton, "Order Management");
+    }
+
+    // Order Management
+    @FXML
+    private Button buyStockButton;
+
+    @FXML
+    private Button sellStockButton;
+
+    @FXML
+    void buyStockClicked(ActionEvent event) {
+        loadFXML("BuyStock.fxml", buyStockButton, "Buy Stock");
+    }
+
+    @FXML
+    void sellStockClicked(ActionEvent event) {
+        loadFXML("SellStock.fxml", sellStockButton, "Sell Stock");
+    }
+
+    // Buy Stock
+    @FXML
+    private Button buyButton;
+
+    @FXML
+    private TextField stockBuyCompanyField;
+
+    @FXML
+    private TextField stockBuyLabelField;
+
+    @FXML
+    private TextField stockMaxPriceField;
+
+    @FXML
+    void buyClicked(ActionEvent event) {
+        String label = stockBuyLabelField.getText();
+        String company = stockBuyCompanyField.getText();
+        double maxPrice = Double.parseDouble(stockMaxPriceField.getText());
+        System.out.println(label + "  " + company + "  " + maxPrice);
+        try {
+            regularUser.buyStockOrder(maxPrice, label, company);
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid Input. You must enter a digit in max.price field");
+        }
+    }
+
+    // Sell Stock
+    @FXML
+    private Button sellButton;
+
+    @FXML
+    private TextField stockMinPriceField;
+
+    @FXML
+    private TextField stockSellCompanyField;
+
+    @FXML
+    private TextField stockSellLabelField;
+
+    @FXML
+    void sellClicked(ActionEvent event) {
+        String label = stockSellLabelField.getText();
+        String company = stockSellCompanyField.getText();
+        double minPrice = Double.parseDouble(stockMinPriceField.getText());
+        System.out.println(label + "  " + company + "  " + minPrice);
+        try {
+            regularUser.sellStockOrder(label, company, minPrice);
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid Input. You must enter a digit in min.price field");
+        }
     }
 
     // // handle errors
