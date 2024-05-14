f2b6fb4f9ff400324f3f9a2cbbdd227dae3e7b09
RaghadAbdelhameed <Raghad.Abdelhameed04@eng-st.cu.edu.eg>
2024-05-14 12:10:33 +0300

    added bond and option

--- Stock/src/Bond.java
+++ Stock/src/Bond.java
@@ -1,51 +1,59 @@
-// import java.time.LocalDate;
-//
-// public class Bond extends Security {
-// private double couponRate; // Annual coupon rate
-// private LocalDate maturityDate; // Date when the bond matures
-// private double faceValue; // Face value of the bond
-//
-// // Constructor
-// public Bond(String symbol, double price, double couponRate, LocalDate
-// maturityDate, double faceValue) {
-// super(symbol, price);
-// this.couponRate = couponRate;
-// this.maturityDate = maturityDate;
-// this.faceValue = faceValue;
-// }
-//
-// // Getters and setters for additional properties
-// public double getCouponRate() {
-// return couponRate;
-// }
-//
-// public void setCouponRate(double couponRate) {
-// this.couponRate = couponRate;
-// }
-//
-// public LocalDate getMaturityDate() {
-// return maturityDate;
-// }
-//
-// public void setMaturityDate(LocalDate maturityDate) {
-// this.maturityDate = maturityDate;
-// }
-//
-// public double getFaceValue() {
-// return faceValue;
-// }
-//
-// public void setFaceValue(double faceValue) {
-// this.faceValue = faceValue;
-// }
-//
-// // Implementation of abstract method
-// @Override
-// public double calculateProfitLoss() {
-// // For bonds, profit/loss calculation may be complex and depends on various
-// factors such as current market interest rates
-// // Here, we can implement a simplified version considering only the
-// difference between face value and current price
-// return getFaceValue() - getPrice();
-// }
-// }
+import java.time.LocalDate;
+
+public class Bond extends Security {
+    private double couponRate; // Annual coupon rate
+    private LocalDate maturityDate; // Date when the bond matures
+    private double faceValue; // Face value of the bond
+
+    // Constructor
+    public Bond(String symbol, double price, double couponRate, LocalDate maturityDate, double faceValue) {
+        super(symbol, price);
+        this.couponRate = couponRate;
+        this.maturityDate = maturityDate;
+        this.faceValue = faceValue;
+    }
+
+    // Getters and setters for additional properties
+    public double getCouponRate() {
+        return couponRate;
+    }
+
+    public void setCouponRate(double couponRate) {
+        this.couponRate = couponRate;
+    }
+
+    public LocalDate getMaturityDate() {
+        return maturityDate;
+    }
+
+    public void setMaturityDate(LocalDate maturityDate) {
+        this.maturityDate = maturityDate;
+    }
+
+    public double getFaceValue() {
+        return faceValue;
+    }
+
+    public void setFaceValue(double faceValue) {
+        this.faceValue = faceValue;
+    }
+
+    // Implementation of abstract method
+    @Override
+    public double calculateProfitLoss() {
+        // For bonds, profit/loss calculation may be complex and depends on various
+        // factors such as current market interest rates
+        // Here, we can implement a simplified version considering only the difference
+        // between face value and current price
+        return getFaceValue() - getPrice();
+    }
+
+    public abstract double getCurrentPrice() {
+    	
+    }
+
+    public abstract void placeOrder(Transaction transaction) {
+    	
+    }
+
+}
