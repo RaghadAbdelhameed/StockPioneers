10c7d5445f287e5418f5cc648ea551d2e1fe1bf7
RaghadAbdelhameed <Raghad.Abdelhameed04@eng-st.cu.edu.eg>
2024-05-16 13:38:23 +0300

    added linecharts,candlestickcharts as scenes

--- Stock/src/zChartingController.java
+++ Stock/src/zChartingController.java
@@ -1,8 +1,11 @@
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
 public class zChartingController extends MainController {
+    private Admin admin;
+    private Stock stock;
 
     @FXML
     private Button candlestickChartsButton;
@@ -11,13 +14,35 @@ public class zChartingController extends MainController {
     private Button lineChartsButton;
 
     @FXML
-    void candlestickChartsClicked(ActionEvent event) {
+    private Button searchButton;
 
+    @FXML
+    private TextField stockLabelField;
+
+    @FXML
+    void candlestickChartsClicked(ActionEvent event) {
+        loadFXML("CandleStickCharts.fxml", candlestickChartsButton, "CandleStick Charts ");
     }
 
     @FXML
     void lineChartsClicked(ActionEvent event) {
+        loadFXML("LineCharts.fxml", lineChartsButton, "Line Charts");
+    }
 
+    @FXML
+    void searchClicked(ActionEvent event) {
+        String label = stockLabelField.getText();
+        stock.setLabel(label);
+        admin.accessStock(label);
+    }
+
+    // Line Charts NOTE: back button is the same for both
+    @FXML
+    private Button backButton;
+
+    @FXML
+    void backClicked(ActionEvent event) {
+        loadFXML("Charts.fxml", backButton, "Charting Options");
     }
 
 }
